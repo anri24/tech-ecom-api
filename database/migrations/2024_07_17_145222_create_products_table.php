304b0bcac2660ddb00d664c73507d0660ba3e1c5
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-07-17 19:06:42 +0400

    create product model migration controller and requests

---
+++ database/migrations/2024_07_17_145222_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('category_id')->on('categories')->references('id');
+            $table->string('name');
+            $table->float('price');
+            $table->integer('discount')->nullable();
+            $table->boolean('in_stock');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
